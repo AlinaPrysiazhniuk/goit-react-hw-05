f7301eb1c8c7a1934a5b7af01ff75003dbed11f5
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2024-02-19 20:53:26 +0200

    api 3

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -3,24 +3,32 @@ import axios from "axios";
 const BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = "19d2d0f24e837c8f3ad6ea805c1086b1";
 
-export default function getTrendingMovies () {
+function getTrendingMovies() {
   return axios.get(`${BASE_URL}/trending/movie/day?api_key=${API_KEY}`);
-};
+}
 
-export default function getMovieDetails (movieId) {
+function getMovieDetails(movieId) {
   return axios.get(`${BASE_URL}/movie/${movieId}?api_key=${API_KEY}`);
-};
+}
 
-export default function getCastsDetails (movieId) {
+function getCastsDetails(movieId) {
   return axios.get(`${BASE_URL}/movie/${movieId}/credits?api_key=${API_KEY}`);
-};
+}
 
-export default function getReviewsDetails  (movieId) {
+function getReviewsDetails(movieId) {
   return axios.get(`${BASE_URL}/movie/${movieId}/reviews?api_key=${API_KEY}`);
-};
+}
 
-export default function getSearchMovie (query) {
+function getSearchMovie(query) {
   return axios.get(
     `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`
   );
+}
+
+export {
+  getCastsDetails,
+  getMovieDetails,
+  getReviewsDetails,
+  getSearchMovie,
+  getTrendingMovies,
 };
