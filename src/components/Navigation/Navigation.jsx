9cf74c9cbec27a063185494ed6596ce80eca8e66
alina prysiazhniuk <alinkaprisiazhnuyk@gmail.com>
2024-02-28 22:32:58 +0200

    navigation

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,23 @@
+import { NavLink } from "react-router-dom";
+import css from "../Navigation/Navigation.module.css";
+
+const Navigation = () => {
+  return (
+    <div className={css.container_header}>
+      <NavLink
+        to="/"
+        className={({ isActive }) => (isActive ? css.active : css.link_header)}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/movies"
+        className={({ isActive }) => (isActive ? css.active : css.link_header)}
+      >
+        Movies
+      </NavLink>
+    </div>
+  );
+};
+
+export default Navigation;
--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,29 +1,12 @@
-import { Suspense } from 'react';
-import { NavLink, Outlet } from 'react-router-dom';
-import css from './SharedLayout.module.css';
+import { Suspense } from "react";
+import { Outlet } from "react-router-dom";
+import Navigation from "../Navigation/Navigation";
 
 export const SharedLayout = () => {
   return (
     <section>
       <header>
-        <div className={css.container_header}>
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              isActive ? css.active : css.link_header
-            }
-          >
-            Home
-          </NavLink>
-          <NavLink
-            to="/movies"
-            className={({ isActive }) =>
-              isActive ? css.active : css.link_header
-            }
-          >
-            Movies
-          </NavLink>
-        </div>
+        <Navigation />
       </header>
       <Suspense fallback={<div>Loading page...</div>}>
         <Outlet />
